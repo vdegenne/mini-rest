4f9381b8639376959901a5d790ede6dac8dc97e5
Valentin Degenne <vdegenne@gmail.com>
2025-02-11 13:43:47 +0100

    0.1.0: precache answer

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,48 @@
+interface RestOptions {
+	/**
+	 * Whether to cache `text` and `json` after the response, so it can be directly retrieve,
+	 *  e.g.
+	 *  ```js
+	 *  const {json} = await get('data')
+	 *  typeof json === 'object' // true
+	 *  ```
+	 *  instead of,
+	 *  ```js
+	 *  const {json} = await get('data')
+	 *  typeof (await json()) === 'object' // true
+	 *  ```
+	 *
+	 *  @default false
+	 */
+	precache: boolean;
+}
+
 export class Rest {
-	constructor(protected baseURL: string) {}
+	#options: RestOptions;
+
+	constructor(
+		protected baseURL: string,
+		options: Partial<RestOptions>,
+	) {
+		this.#options = Object.assign(
+			{},
+			{precache: false} as RestOptions,
+			options,
+		);
+	}
 
 	get(path = '/') {
-		return get(`${this.baseURL}/${path}`);
+		return get(`${this.baseURL}/${path}`, this.#options.precache);
 	}
 	post(path = '/', body: any) {
-		return post(`${this.baseURL}/${path}`, body);
+		return post(`${this.baseURL}/${path}`, body, this.#options.precache);
 	}
 }
 
-export async function get(url: string) {
+export async function get(
+	url: string,
+	precache = false,
+): Promise<ResponseObject> {
 	if (!url.startsWith('http')) {
 		url = `http://${url}`;
 	}
@@ -19,16 +52,24 @@ export async function get(url: string) {
 	return {
 		response,
 		status: response.status,
-		async json() {
-			return await response.json();
-		},
-		async text() {
-			return await response.text();
-		},
+		text: precache
+			? await response.text()
+			: async function () {
+					return await response.text();
+				},
+		json: precache
+			? await response.json()
+			: async function () {
+					return await response.json();
+				},
 	};
 }
 
-export async function post(url: string, body: any) {
+export async function post(
+	url: string,
+	body: any,
+	precache = false,
+): Promise<ResponseObject> {
 	if (!url.startsWith('http')) {
 		url = `http://${url}`;
 	}
@@ -42,11 +83,22 @@ export async function post(url: string, body: any) {
 	return {
 		response,
 		status: response.status,
-		async json() {
-			return await response.json();
-		},
-		async text() {
-			return await response.text();
-		},
+		text: precache
+			? await response.text()
+			: async function () {
+					return await response.text();
+				},
+		json: precache
+			? await response.json()
+			: async function () {
+					return await response.json();
+				},
 	};
 }
+
+type ResponseObject = {
+	response: Response;
+	status: number;
+	text: string | (() => Promise<string>);
+	json: any | (() => Promise<any>);
+};
